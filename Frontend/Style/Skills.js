33e141b4e55adf6d55f85f9b050e8a145251ff20
André Gomes Moura <andregmoura06@gmail.com>
2025-05-08 10:19:39 -0300

    feat: component Skiils Adicionado

--- Frontend/Style/Skills.js
+++ Frontend/Style/Skills.js
@@ -4,7 +4,7 @@ import { Color } from "./Components.js";
 
 
 export const SectionSkills = styled.section`
-  
+  display:block;
   padding: 80px 20px;
   text-align: center;
   box-shadow: 0 0 10px rgba(0, 0, 0, 0.05);
@@ -15,10 +15,13 @@ export const SectionSkills = styled.section`
   }
 
   h1 {
+    color :${Color.Colors.text};
     font-size: 50px;
     font-weight: 600;
-    margin-left: -65%;
-    margin-top: -3%;
+    text-align: center;
+    margin-left: -32%;
+    margin-top: -4%;
+
     
     @media (max-width: 768px) {
       margin-left: 0px;
@@ -27,9 +30,47 @@ export const SectionSkills = styled.section`
 }
 `;
 
-export const Titulo = styled.h1`
-    color :${Color.Colors.text};
-    margin-left: -67%;
-    margin-top: -5%;
+
+
+export const Base = styled.div`
+  display: flex;
+  background-color: ${Color.Colors.Primary};
+  width: 400px;
+  height: 400px;
+  border-top-right-radius: 130px;
+  border-bottom-left-radius: 130px;
+  margin-left: 300px;
+  margin-top: 30%;
+
+  flex-direction: column;
+  justify-content: center;
+
+  h4{
+    white-space: nowrap;
+  
+    margin-bottom: 20%;
+    font-size: 30px;
+  }
+
+  p{
+    margin-left: -50%;
+  }
+
+
+
+
+
+  @media (max-width: 768px) {
+    margin-left: 0px;
+    margin-top: 10%;
+    max-width: 250px;
+    height: 250px;
+    border-radius: 30px;
+
+  }
+
+  &:hover {
+  
+}
 
 `;
