cf992160aa8a4887af0bd4158c7312992183d933
André Gomes Moura <andregmoura06@gmail.com>
2025-05-11 10:26:06 -0300

    fix: Corrigido o campo de Skills que ficava saindo da tela e feito a responsividade

--- Frontend/Pages/home.jsx
+++ Frontend/Pages/home.jsx
@@ -7,9 +7,18 @@ import {
   MyImg,
   Main,
   Section,
-  StyledButton
+  StyledButton,
+  Color
 } from "../Style/Components";
+import {
+  SectionSkills,
+  Base,
+  AjustarBase
+
+} from "../Style/Skills"
+import { MdOutlineCircle } from "react-icons/md";
 import { FaGithub, FaLinkedin } from "react-icons/fa";
+
 
 
 const Home = () => {
@@ -42,7 +51,38 @@ const Home = () => {
             </Moldura>
           </div>
         </Section>
+        <SectionSkills className="row justify-content-center align-items-center">
+          <h1>Skills</h1>
+          <AjustarBase className="col-md-5 d-flex flex-column flex-md-row  mt-4 mt-md-0  align-items-center " >
+            <Base>
+              <h4>Linguagens e Tecnologias</h4>
+              <div className="skills-list">
+                <div className="skill-item"><MdOutlineCircle />HTML5</div>
+                <div className="skill-item"><MdOutlineCircle />CSS</div>
+                <div className="skill-item"><MdOutlineCircle />JavaScript</div>
+              </div>
+            </Base>
+            <Base>
+              <h4>Frameworks e Bibliotecas</h4>
+              <div className="skills-list">
+                <div className="skill-item"><MdOutlineCircle />React.js</div>
+                <div className="skill-item"><MdOutlineCircle />Bootstrap</div>
+                <div className="skill-item"><MdOutlineCircle />Styled Components</div>
+              </div>
+            </Base>
+            <Base>
+              <h4>Ferramentas e Práticas</h4>
+              <div className="skills-list">
+                <div className="skill-item"><MdOutlineCircle /> Git/GitHub</div>
+                <div className="skill-item"><MdOutlineCircle />Consumo de APIs REST </div>
+                <div className="skill-item"><MdOutlineCircle />Responsividade</div>
+              </div>
+            </Base>
+          </AjustarBase>
+
+        </SectionSkills>
       </Main>
+
 
       <footer className="text-center py-3 bg-light text-muted">
         <p>&copy; 2025 - Todos os direitos reservados</p>
